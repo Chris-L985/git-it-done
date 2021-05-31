3e1df26f675e590a4af6bed3519d3f475f8da89e
Chris-L985 <thelopezdispenser@gmail.com>
2021-05-31 13:31:25 -0600

    added query to link

--- assets/js/single.js
+++ assets/js/single.js
@@ -2,6 +2,22 @@ var repoNameEl = document.querySelector("#repo-name");
 var issueContainerEl = document.querySelector("#issues-container");
 var limitWarningEl = document.querySelector("#limit-warning");
 
+let getRepoName = function() {
+    var queryString = document.location.search;
+    var repoName = queryString.split("=") [1];
+    getRepoIssues(repoName);
+
+    if(repoName) {
+        repoNameEl.textContent = repoName;  
+        
+        getRepoIssues
+    } else {
+        document.location.replace("./index.html");
+    } 
+};
+
+var queryString = document.location.search;
+
 var getRepoIssues = function(repo) {
 
     var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
@@ -19,8 +35,7 @@ var getRepoIssues = function(repo) {
             });
         }
         else {
-            console.log(response);
-            alert("There was a problem with your request!");
+            document.location.replace("./index.html");
         }
     });
 };
@@ -76,4 +91,4 @@ var displayWarning = function(repo) {
     limitWarningEl.appendChild(linkEl);
 };
 
-getRepoIssues("facebook/react");
+getRepoName();
